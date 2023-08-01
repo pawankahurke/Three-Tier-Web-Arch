537fd5455a8b6b89d3cfd210fb76870915911963
pawankahurke <137269488+pawankahurke@users.noreply.github.com>
2023-08-01 17:11:06 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'webapp',
+    DB_USER : 'admin',
+    DB_PWD : 'admin123',
+    DB_DATABASE : 'database-1.cluster-c30qc9s7dn8o.us-east-1.rds.amazonaws.com'
 });
